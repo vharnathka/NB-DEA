10152cadb773bddd3312e31fc13ca0ed41f51abb
vharnathka <vharnathka@ucsd.edu>
2024-05-30 07:04:00 +0000

    setup

--- setup.py
+++ setup.py
@@ -1,3 +1,20 @@
+import os
+from setuptools import setup, find_packages
+
+# version-keeping code based on pybedtools
+curdir = os.path.abspath(os.path.dirname(__file__))
+MAJ = 0
+MIN = 0
+REV = 0
+VERSION = '%d.%d.%d' % (MAJ, MIN, REV)
+with open(os.path.join(curdir, 'mypileup/version.py'), 'w') as fout:
+        fout.write(
+            "\n".join(["",
+                       "# THIS FILE IS GENERATED FROM SETUP.PY",
+                       "version = '{version}'",
+                       "__version__ = version"]).format(version=VERSION)
+        )
+        
 setup(
     name='nb-dea',
     version=VERSION,
