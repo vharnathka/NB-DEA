392fa891fbc90cb7aba4e04eb08e4909a33c2b1d
vharnathka <vharnathka@ucsd.edu>
2024-05-30 07:16:58 +0000

    setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ MAJ = 0
 MIN = 0
 REV = 0
 VERSION = '%d.%d.%d' % (MAJ, MIN, REV)
-with open(os.path.join(curdir, 'nb-dea/version.py'), 'w') as fout:
+with open(os.path.join(curdir, 'nb_dea/version.py'), 'w') as fout:
         fout.write(
             "\n".join(["",
                        "# THIS FILE IS GENERATED FROM SETUP.PY",
@@ -24,7 +24,7 @@ setup(
     packages=find_packages(),
     entry_points={
         "console_scripts": [
-            "nb-dea=nb-dea.nb-dea:main"
+            "nb-dea=nb_dea.nb_dea:main"
         ],
     },
 )
