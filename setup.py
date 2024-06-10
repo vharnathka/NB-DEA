b3045f5d48aeef1d2a244ece4ba2ae37ab4eef76
vharnathka <122574933+vharnathka@users.noreply.github.com>
2024-06-09 21:00:18 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
 import os
 from setuptools import setup, find_packages
 
+curdir = os.path.abspath(os.path.dirname(__file__))
+MAJ = 0
+MIN = 0
+REV = 0
+VERSION = '%d.%d.%d' % (MAJ, MIN, REV)
+with open(os.path.join(curdir, 'nb_dea/version.py'), 'w') as fout:
+        fout.write(
+            "\n".join(["",
+                       "# THIS FILE IS GENERATED FROM SETUP.PY",
+                       "version = '{version}'",
+                       "__version__ = version"]).format(version=VERSION)
+        )
 setup(
     name='nb_dea',
     version='0.0.0',
